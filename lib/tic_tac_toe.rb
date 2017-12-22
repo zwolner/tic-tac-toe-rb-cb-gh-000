01cc5192830d24fb55a087d81a51fee23d6d9e7e
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-22 17:58:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,6 +78,7 @@ end
 
 def turn(board)
   puts "Please enter a move 1-9:"
+  player_token = current_player(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
